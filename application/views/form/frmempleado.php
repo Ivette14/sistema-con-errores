9aebdc82d277582587bd445a953eb81d8c934b18
jrobcastro <jrobcastro@live.com>
2014-05-13 12:31:43 -0600

    form, editar, agregar
    area, traslado, sucursa y empleado

--- application/views/form/frmempleado.php
+++ application/views/form/frmempleado.php
@@ -10,46 +10,47 @@
         </div><!-- /.row -->
 
             <!-- /.row -->
-        <div class="form-group"> <button type="button" onclick=location="<?php echo base_url().'direccion/hrefa_emplea'; ?>" class="btn btn-primary"><i class="glyphicon glyphicon-plus"></i>&nbsp;Agregar</button></div>       
+        <div class="form-group"> <button type="button" onclick=location="<?php echo base_url().'crud_empleado/agregar'; ?>" class="btn btn-primary"><i class="glyphicon glyphicon-plus"></i>&nbsp;Agregar</button></div>       
             <div class="row">
                 <div class="col-lg-12">
                     <div class="panel panel-default">
                         <!-- /.panel-heading -->
-                      <form action="" id="tabla_sucursal" method="post" role="form">
+                      
                         <div class="panel-body">
                             <div class="table-responsive">
                                 <table class="table table-striped table-bordered table-hover" id="dataTables-example">
                                     <thead>
                                         <tr>
-                                            <th>Nombre de la Sucursal</th>
-                                            <th>Telefono</th>
+                                            <th>Sucursal</th>
+                                            <th>Nombre del Empleado</th>
                                             <th>Direccion</th>
-                                            <th>Departamento</th>
+                                            <th>Telefono</th>
+                                            <th>Email</th>
                                             <th>Editar</th>
                                             <th>Eliminar</th>
                                         </tr>
                                     </thead>
                                     <tbody>
-                                            <?php if(isset($sucursales)){ ;?>
-                                            <?php foreach ($sucursales as $sucursal):?>
+                                            <?php foreach ($empleados as $empleado):?>
                                             <tr>
-                                            <td><?= $sucursal->nombre_sucursal?></td>   
-                                            <td><?= $sucursal->telefono_sucursal?></td> 
-                                            <td><?= $sucursal->direccion_sucursal?></td> 
-                                            <td><?= $sucursal->departamento?></td>
-                                            <td><a href="<?= base_url().'crud/actualizar_sucursal/'.$sucursal->id_sucursal?>">Editar</a></td>
-                                            <td><a href="<?= base_url().'crud/eliminar_sucursal/'.$sucursal->id_sucursal?>">Eliminar</a></td> 
+                                            <td><?= $empleado->id_sucursal?></td>                                      
+                                            <td><?= $empleado->nombre_empleado?></td> 
+                                            <td><?= $empleado->direccion_empleado?></td> 
+                                            <td><?= $empleado->telefono_empleado?></td>
+                                            <td><?= $empleado->email_empleado?></td>
+                                            <td align="center"><button type="button" onclick=location="<?php echo base_url().'crud_empleado/editar/'.$empleado->id_empleado; ?>" class="btn btn-primary"><i class="glyphicon glyphicon-pencil"></i>&nbsp;Editar</button></td>
+                                            <td align="center"><button type="button" onclick=location="<?php echo base_url().'crud_empleado/eliminar/'.$empleado->id_empleado; ?>" class="btn btn-danger"><i class="glyphicon glyphicon-trash"></i>&nbsp;Eliminar</button></td> 
                                             </tr>
                                             <?php endforeach ;?>
-                                            <?php }; ?>
-                                    
+                                         
+
                                     </tbody>
                                 </table>
                             </div>
                             <!-- /.table-responsive -->                           
                         </div>
                         <!-- /.panel-body -->
-                     </form>
+                     
                     </div>
                     <!-- /.panel -->
                 </div>
